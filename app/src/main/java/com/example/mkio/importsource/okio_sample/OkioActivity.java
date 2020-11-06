cfdbda80e000a7f75b017cdff63fef3905c94c3c
liguoying <15510224541@163com>
2020-11-06 15:47:06 +0800

    add okio sourcecode

---
+++ app/src/main/java/com/example/mkio/importsource/okio_sample/OkioActivity.java
@@ -0,0 +1,15 @@
+package com.example.mkio.importsource.okio_sample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.mkio.importsource.R;
+
+public class OkioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_okio);
+    }
+}
